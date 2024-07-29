52e4aa460934dc26f8089184de390527277a5a66
Kishan_Gupta <guptakishan428@gmail.com>
2024-07-29 20:48:02 +0530

    initial commit

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(express.json({ extended: false }));
 // Define Routes
 app.use("/api/admin", require("./src/routes/admin"));
 app.use("/api/user", require("./src/routes/user"));
+app.get("/", (req, res) => {
+  res.send("Welcome to the Web Development Assignment API");
+});
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
